0621c3908aead4e78913a9e47957976d84b2c3af
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-09-29 22:33:10 +0200

    feat(homepage): update style

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit, AfterViewInit {
+
+  // ref main_vdo in background
+  @ViewChild('vdo')
+  el_vdo!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {   
+    this.el_vdo.nativeElement.playbackRate = 1 // (default 1) 
   }
 
 }
