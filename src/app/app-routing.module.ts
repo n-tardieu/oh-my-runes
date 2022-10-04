fc43590e2a44cb7ea63c7be132887198449b76d1
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-10-05 00:36:13 +0200

    fix(routing): delete /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
-  {path: '/me', component: DashboardComponent},
+  {path: 'me', component: DashboardComponent},
   {path: '**', component: HomepageComponent}
 ];
 
