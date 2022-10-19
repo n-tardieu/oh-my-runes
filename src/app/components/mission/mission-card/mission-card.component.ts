8ae74ef101f6a1d42da9bef8630b77fa440b190b
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-10-19 23:01:48 +0200

    feat(mission-card): add getTitle & getNumberRomanize

--- src/app/components/mission/mission-card/mission-card.component.ts
+++ src/app/components/mission/mission-card/mission-card.component.ts
@@ -16,4 +16,21 @@ export class MissionCardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getTitle(m: Mission) {
+    return `${m.title} ${this.getNumberRomanize(m.missionLevel)} (${m.avancementCount}/${m.target})`
+  }
+
+  // TODO
+  getNumberRomanize(number: number) {
+    if (number == 1) return 'I'
+    else if (number == 2) return 'II'
+    else if (number == 3) return 'III'
+    else if (number == 4) return 'IV'
+    else if (number == 5) return 'V'
+    else if (number == 6) return 'VI'
+    else if (number == 7) return 'VII'
+    else if (number == 8) return 'VIII'
+    else return '0'
+  }
+
 }
