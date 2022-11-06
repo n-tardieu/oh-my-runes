286e8e69e6d3eba3086f14266008b21df43f8188
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-11-06 23:29:35 +0100

    feat(app-routing): add params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EnchantsAndGemsParamsComponent } from './components/enchants-and-gems-params/enchants-and-gems-params.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'params', component: EnchantsAndGemsParamsComponent},
   {path: '**', component: HomepageComponent}
 ];
 
