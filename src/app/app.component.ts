6a360451a98a4a3de04536a20d65b2e6745393f0
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-11-07 08:44:32 +0100

    feat(app): start navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Wizard } from './core/types/sw-wizard.types';
+import { WizardService } from './services/wizard.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ohmyrunes';
+  public wizardHasSet: boolean = false;
+
+  wizardSubscription: Subscription = new Subscription
+
+  constructor(private wizardService: WizardService) { }
+
+  ngOnInit() {
+    if (this.wizardService.getWizardData() !== undefined) this.wizardHasSet = true
+    this.wizardSubscription = this.wizardService.wizardSubject$.subscribe((wizard: Wizard) => {
+      if (wizard == undefined) {
+        this.wizardHasSet = false
+      } else {
+        this.wizardHasSet = true
+      }
+    })
+  }
 }
