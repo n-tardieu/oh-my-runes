152e61f7cddac0ca83dbe2ca7920a688546ce8fb
Nicolas Tardieu <ntardieu.contact@gmail.com>
2022-11-07 08:43:10 +0100

    feat(enchantsAndGemsParamsComponent): paramsSubscription

--- src/app/components/enchants-and-gems-params/enchants-and-gems-params.component.ts
+++ src/app/components/enchants-and-gems-params/enchants-and-gems-params.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, KeyValueDiffer, KeyValueDiffers, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { RunesListParams } from 'src/app/core/interfaces/runes-list-params.interfaces';
 import { RunesConvertService } from 'src/app/services/runes-convert.service';
 import { WizardService } from 'src/app/services/wizard.service';
@@ -13,23 +14,26 @@ export class EnchantsAndGemsParamsComponent implements OnInit {
   isValid = true;
 
   @Input()
-  public params: RunesListParams = {
-    isEquipedRunes: true,
-    isOnlyStorageRunes: true,
-    isAbort: true,
-    gemGrade: 'default'
-  }
+  public params!: RunesListParams
 
   private paramsDiffer!: KeyValueDiffer<string, any>;
+  paramsSubscription: Subscription = new Subscription
+
 
   constructor(private differs: KeyValueDiffers, private runesConvertService: RunesConvertService, private wizardService: WizardService) { }
 
   ngOnInit() {
+    this.params = this.runesConvertService.getRunesListParams()
+    this.paramsSubscription = this.runesConvertService.runesListParamsSubject$.subscribe((params: any) => {
+      this.params = params 
+    })
+
     this.paramsDiffer = this.differs.find(this.params).create();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log("changes ", changes);
+    this.runesConvertService.setRunesListParams(this.params)
   }
 
   ngDoCheck(): void {
